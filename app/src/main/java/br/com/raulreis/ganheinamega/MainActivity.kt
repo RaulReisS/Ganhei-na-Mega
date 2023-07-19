15e6698d34270249839eadeb6c2d8275510c365b
Raul Reis <raul.soares@usp.br>
2023-07-19 02:49:37 -0300

    Cirada a lógia de programção para gerar os números do jogo.

--- app/src/main/java/br/com/raulreis/ganheinamega/MainActivity.kt
+++ app/src/main/java/br/com/raulreis/ganheinamega/MainActivity.kt
@@ -2,12 +2,77 @@ package br.com.raulreis.ganheinamega
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Buscar referência dos objetos
+        val edtText = findViewById<EditText>(R.id.edtNumber)
+        val txvResult = findViewById<TextView>(R.id.txvResult)
+        val btnGenerate = findViewById<Button>(R.id.btnGenerate)
 
-     }
+        // Opção 1: Evento de touch por XML
+
+        // Opção 2: variavel que seja do tipo View.OnClickListener (interface)
+
+        // btnGenerate.setOnClickListener(buttonClickListeber)
+
+        // !Opção 3: Mais simples: bloco de código que será disparado pleo onClick Listener
+
+        btnGenerate.setOnClickListener {
+
+            val text = edtText.text.toString()
+
+            numberGenerator(text, txvResult)
+        }
+    }
+
+    private fun numberGenerator(text: String, txtResult: TextView) {
+        // Validar quando o campo é vazio
+        if (text.isNotEmpty()) {
+
+            val qtd = text.toInt()
+
+            if (qtd in 6..15) {
+
+                val numbers = mutableSetOf<Int>()
+                val random = Random()
+
+                while(true) {
+                    val number = random.nextInt(60) // 0 .. 59
+                    numbers.add(number + 1)
+
+                    if (numbers.size == qtd)
+                        break
+                }
+
+                txtResult.text = numbers.joinToString(" - ")
+            }
+            else {
+                Toast.makeText(this, R.string.number_hint, Toast.LENGTH_LONG).show()
+            }
+        } else {
+            Toast.makeText(this, R.string.number_hint, Toast.LENGTH_LONG).show()
+        }
+
+        // validar se o campo informado é entre 6 e 15
+    }
+
+    //val buttonClickListeber = object :View.OnClickListener {
+      //  override fun onClick(p0: View?) {
+            //Ação
+        //}
+    //}
+
+//    fun buttonClicked(view: View) {
+        //Ação
+//    }
 }
